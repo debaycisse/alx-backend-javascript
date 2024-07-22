69caea5308e365c34430e23ab15212c6c4aa3cf0
Azeez Adebayo <debaycisse@gmail.com>
2024-07-22 15:46:30 -0700

    refactor code to use a single line

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,1 @@
-export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
-
-  return budget;
-}
+export default (income, gdp, capita) => ({ income, gdp, capita });
