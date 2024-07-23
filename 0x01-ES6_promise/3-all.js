9fd9abdf4c6e0ccacb220b8d5155a83293a1affe
Azeez Adebayo <debaycisse@gmail.com>
2024-07-23 21:07:52 +0100

    return the promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([createUser(), uploadPhoto()])
+  return Promise
+    .all([createUser(), uploadPhoto()])
     .then((userInfo) => {
       console.log(
         `${userInfo[1].body} ${userInfo[0].firstName} ${userInfo[0].lastName}`,
