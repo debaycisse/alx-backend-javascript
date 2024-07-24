89b6183aae9ed5eafef7285e3ff9d2af3e3044b9
Azeez Adebayo <debaycisse@gmail.com>
2024-07-24 13:22:52 +0100

    handle only the resolved promise

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,6 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise
     .any([chinaDownload, USDownload])
-    .then((data) => data)
-    .catch((error) => error);
+    .then((data) => data);
 }
