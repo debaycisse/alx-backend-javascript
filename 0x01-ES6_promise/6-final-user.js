2b5fed58e51bfc363ffa5e9c893de14f0b71d700
Azeez Adebayo <debaycisse@gmail.com>
2024-07-24 12:17:44 +0100

    apply a workaround to capture error's reason and fulfilled data

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => [{ status: result.status, value: result }])
+  return Promise
+    .allSettled([
+      signUpUser(firstName, lastName), uploadPhoto(fileName),
+    ])
+    .then((allData) => allData.map((eachData) => ({
+      status: eachData.status,
+      value: eachData.status === 'rejected' ? eachData.reason : eachData.value,
+    })))
     .catch((error) => [{ status: error.status, value: error.value }]);
 }
