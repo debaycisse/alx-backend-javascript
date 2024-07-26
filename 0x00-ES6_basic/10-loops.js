18b00b7bd0f8b3ca398ad36ed6daf851b54f5c80
Azeez Adebayo <debaycisse@gmail.com>
2024-07-26 14:38:56 +0100

    change from for...in to for...of loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
-  for (const idx in array) {
-    if (Object.hasOwn(array, idx)) {
-      const value = array[idx];
-      newArray.push(appendString + value);
-    }
+  for (const element of array) {
+    newArray.push(appendString + element);
   }
 
   return newArray;
