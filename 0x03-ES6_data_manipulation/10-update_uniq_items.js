dc676cee9142ecbefd237cfa208e551a4abe77b4
Azeez Adebayo <debaycisse@gmail.com>
2024-07-30 12:44:13 +0100

    throw error when an attempt to update map fails

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,11 @@
 export default function updateUniqueItems(mapOfItems) {
   for (const [key, value] of mapOfItems) {
     if (value === 1) {
-      mapOfItems.set(key, 100);
+      try {
+        mapOfItems.set(key, 100);
+      } catch(e) {
+        throw Error('Cannot process');
+      }
     }
   }
 }
