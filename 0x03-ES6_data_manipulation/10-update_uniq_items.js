87f222bf318a4ca482377791cbe3842e6f35f347
Azeez Adebayo <debaycisse@gmail.com>
2024-07-30 12:50:10 +0100

    relocate the try block

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 export default function updateUniqueItems(mapOfItems) {
-  for (const [key, value] of mapOfItems) {
-    if (value === 1) {
-      try {
+  try {
+    for (const [key, value] of mapOfItems) {
+      if (value === 1) {
         mapOfItems.set(key, 100);
-      } catch (e) {
-        throw Error('Cannot process');
       }
     }
+  } catch (e) {
+    throw Error('Cannot process');
   }
 }
