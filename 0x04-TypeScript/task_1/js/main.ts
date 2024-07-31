b0f74c8c5578e4df42ef060fd44191fdd73189fb
Azeez Adebayo <debaycisse@gmail.com>
2024-07-31 20:37:43 +0100

    implement task 4
    4. Writing a class
    Add a class whose properties, methods, and contructor are
    described via two interfaces, one for the class, its properties,
    and methods and the other for the constructor of the class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -49,3 +49,51 @@ const printTeacher: printTeacherFunction = (
 
 // Testing the function
 console.log(printTeacher("Azeez", "Adebayo"));
+
+// Task 4
+
+/**
+ * Interface for the StudentClass.
+ * It is implemented by the StudentClass
+ */
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  workOnHomework(): string;
+
+  displayName(): string;
+}
+
+/**
+ * Interface for the contructor.
+ * It is used when calling the constructor of the StudentClass.
+ */
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+/**
+ * The StudentClass that implements the StudentInterface
+ */
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Testing the class and interface
+const studentConstructor: StudentConstructor = StudentClass;
+const student1: StudentInterface = new studentConstructor("Bosun", "Olayemi");
