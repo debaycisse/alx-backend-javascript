92fa151508e16dbeaae61eda5479d166a1ada71e
Azeez Adebayo <debaycisse@gmail.com>
2024-07-31 21:55:04 +0100

    implement task 7
    7. String literal types
    Add a string literal union and use it in a function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -95,3 +95,21 @@ function executeWork(employee: Director | Teacher): string {
 // Tesing task 6
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
+
+
+
+// Task 7
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects) {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else if (todayClass === 'History') {
+        return 'Taching History';
+    }
+}
+
+// Testing task 7
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
