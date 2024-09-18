4782dccdfcb714c3ebfcc8add9c0bd2de20d9e2b
Azeez Adebayo <debaycisse@gmail.com>
2024-09-18 13:47:24 +0100

    implement task 6
    implement a small HTTP server using express module

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const port = 1245;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+module.exports = app;
