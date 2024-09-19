9fb92012645c4245ea60c972b8121028d0f04a22
Azeez Adebayo <debaycisse@gmail.com>
2024-09-19 06:13:12 +0100

    work in progress

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -20,8 +20,21 @@ class StudentsController {
     response.setHeader('Content-Type', 'text/plain');
     try {
       response.statusCode = 200;
-      const major = request.query.major;
-
+      const major = request.params.major;
+      if (major !== 'CS' && major !== 'SWE') {
+        response.statusCode = 500;
+        response.send('Major parameter must be CS or SWE');
+      }
+      let resMsg = 'This is the list of our students\n';
+      const dbData = readDatabase(filePath);
+      const dbDataSplit = dbData.split('\n');
+      for (const message of dbDataSplit) {
+        if (message.includes(major)) {
+          resMsg += message;
+          break;
+        }
+      }
+      response.send(resMsg);
     } catch (error) {
       response.statusCode = 500;
       response.send('Database cannot be loaded');
