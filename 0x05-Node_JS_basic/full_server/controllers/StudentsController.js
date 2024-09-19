441fa7c5801ccfc6b67957eee0359e2a91527594
Azeez Adebayo <debaycisse@gmail.com>
2024-09-19 05:58:08 +0100

    work in progress

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,7 +4,29 @@ const filePath = process.argv[2];
 
 class StudentsController {
   static getAllStudents(request, response) {
-    response.statusCode = 200;
-    readDatabase(filePath)
+    response.setHeader('Content-Type', 'text/plain');
+    try {
+      response.statusCode = 200;
+      let resMsg = 'This is the list of our students\n';
+      resMsg += readDatabase(filePath);
+      response.send(resMsg);
+    } catch (error) {
+      response.statusCode = 500;
+      response.send('Database cannot be loaded');
+    }
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    response.setHeader('Content-Type', 'text/plain');
+    try {
+      response.statusCode = 200;
+      const major = request.query.major;
+
+    } catch (error) {
+      response.statusCode = 500;
+      response.send('Database cannot be loaded');
+    }
   }
 }
+
+module.exports = StudentsController;
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,6 +1,5 @@
-const { count } = require('console');
-
-const fs = require('fs').promises
+const fs = require('fs').promises;
+const countStudents = require('../3-read_file_async');
 
 /**
  * 
@@ -8,41 +7,15 @@ const fs = require('fs').promises
  * @returns - a list of firstnames and fields
  */
 const readDatabase = async (filePath) => {
-  try {
-    const data = await fs.readFile(filePath);
-    const dataSplit = data.split('\n');
-    const studentInfo = [{'CS': {count: 0, names: []}}];
-    const i = 1;
-    while (i < dataSplit.length) {
-      if (dataSplit[i] !== undefined) {
-        let found = null;
-        const _key = dataSplit[i][3];
-        for (const info of studentInfo) {
-          if (_key in info) {
-            found = i;
-            break;
-          }
-        }
-        if (found !== null) {
-          const stdName = dataSplit[i][0];
-          studentInfo[found][count] += 1;
-          studentInfo[found][names].push(stdName);
-        } else {
-          const keyInfo = {
-            _key: {count: 1, names: [dataSplit[i][0]]}
-          };
-          studentInfo.push(keyInfo);
-        }
-      }
+  countStudents(filePath)
+    .then((data) => {
+      const dataSplit = data.split('\n');
+      return dataSplit.slice(1);
+    })
+    .catch((error) => {
+      throw new Error('Database cannot be loaded');
+    });
 
-
-      i += 1;
-    }
-    
-    return studentInfo;
-  } catch (error) {
-    throw new Error('Database cannot be loaded');
-  }
 };
 
 module.exports = readDatabase;
